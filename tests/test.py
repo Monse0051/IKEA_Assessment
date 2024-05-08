6e55cd9548b41ddeccb38dbb61c13a3c7564db4a
Monsse Vargas <rouge0051@gmail.com>
2024-05-08 19:54:27 +0200

    Code template of testing in appium
    Saving a template of testing in appium.
    This code opens android setting.

---
+++ tests/test.py
@@ -0,0 +1,32 @@
+import unittest
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+
+capabilities = dict(
+    platformName='Android',
+    automationName='uiautomator2',
+    deviceName='Android',
+    appPackage='com.android.settings',
+    appActivity='.Settings',
+    language='en',
+    locale='US'
+)
+
+appium_server_url = 'http://localhost:4723'
+
+class TestAppium(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
+
+    def tearDown(self):
+        if self.driver:
+            self.driver.quit()
+
+    def test_find_battery(self):
+        print("hola")
+        #el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
+        #el.click()
+
+if __name__ == '__main__':
+    unittest.main()
