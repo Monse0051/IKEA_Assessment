fe8173f6c08721b0a7f5b2147cd20001f82cff05
Monsse Vargas <rouge0051@gmail.com>
2024-05-08 22:11:32 +0200

    Simple test that opens IKEA app
    Just getting started with a functinal test
    that only opens IKEA app and clicks a button

--- tests/test.py
+++ tests/test.py
@@ -2,16 +2,19 @@ import unittest
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 from appium.webdriver.common.appiumby import AppiumBy
+import time
 
 capabilities = dict(
     platformName='Android',
+    platformVersion='13',
     automationName='uiautomator2',
     deviceName='Android',
-    appPackage='com.android.settings',
-    appActivity='.Settings',
+    appPackage = 'com.ikea.inter.homesmart.system2',
+    appActivity= 'com.ikea.system2.start.StartActivity',
     language='en',
     locale='US'
 )
+
 
 appium_server_url = 'http://localhost:4723'
 
@@ -23,10 +26,13 @@ class TestAppium(unittest.TestCase):
         if self.driver:
             self.driver.quit()
 
-    def test_find_battery(self):
-        print("hola")
-        #el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
-        #el.click()
+    def test_ikea_app(self):
+        time.sleep(0.5)
+        button = self.driver.find_element(AppiumBy.CLASS_NAME, "android.widget.Button")
+        # self.driver.page_source # XML string with the page source
+        print(button)
+        button.click()
+        time.sleep(2)
 
 if __name__ == '__main__':
     unittest.main()
